8d600b23db430205d850c90cce6a8f17cd5c213a
kncpem <k.naik122003@gmail.com>
2024-08-02 14:07:36 +0530

    app updates

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_conversational_chain():
     return chain
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization = True)
     docs = new_db.similarity_search(user_question)
